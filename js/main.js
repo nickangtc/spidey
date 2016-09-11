a0d6c29aa5c8c33824bc548dda21cb5de3b5dcf1
nickangtc <nickangtc@gmail.com>
2016-09-11 22:35:27 +0800

    JS: finished Wikipedia GET and page append

--- js/main.js
+++ js/main.js
@@ -4,21 +4,59 @@ console.log('javascript working');
 
 $(document).ready(function () {
   var SEARCH_STR = '';
+  var RESULTS_LIMIT = 5;
 
-  // submit via search button
+  // ======= Form handler ========
   $('#submit-btn').on('click', function (ev) {
     ev.preventDefault();
     SEARCH_STR = $('form input').val();
     console.log('search string stored as: ' + SEARCH_STR);
-    $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + SEARCH_STR,
-      dataType: 'jsonp',
-      type: 'GET',
-      success: loadResults
-    });
+
+    // show search term on page
+    $('#search-term').text(SEARCH_STR);
+
+    // send GET request to APIs
+    getJSON('jsonp', 'wikipedia');
   });
 
-  function loadResults (data) {
-    console.log(data);
+  // ========== Functions ===========
+  function getJSON (dataType, source) {
+    var url = '';
+    if (source === 'wikipedia') {
+      url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
+    }
+    console.log('url', url, 'source', source, 'searchstr', SEARCH_STR);
+
+    $.ajax({
+      url: url + SEARCH_STR,
+      dataType: dataType,
+      type: 'GET',
+      success: function (data) {
+        loadResults(data, source);
+      }
+    });
+  }
+
+  function loadResults (data, source) {
+    console.log(data, source);
+    for (var i = 0; i < RESULTS_LIMIT; i++) {
+      var url = data[3][i];
+      var row = $('<div>').addClass('row');
+      var a = $('<a>').attr('href', url);
+      var col = $('<div>').addClass('col-sm-12 result-card');
+      var title = $('<h4>').text(data[1][i]);
+      var excerpt = $('<p>').text(data[2][i]);
+
+      col.append(title, excerpt);
+      a.append(col);
+      row.append(a);
+      $('#wikipedia').append(row);
+    }
+    // create div class row
+    // create and append to div: a href "wiki URL"
+    // create and append to a: div class col-sm-12 result-card
+    // create and append to div: h4 for title, p for excerpt
+    // append to $('#wikipedia')
+
   }
 });
