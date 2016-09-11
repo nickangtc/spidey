a45305442335e30a36e7a54e40b68461db229117
nickangtc <nickangtc@gmail.com>
2016-09-11 20:52:36 +0800

    JS: hooked up submit btn to GET wikipedia opensearch API

--- js/main.js
+++ js/main.js
@@ -3,5 +3,22 @@
 console.log('javascript working');
 
 $(document).ready(function () {
+  var SEARCH_STR = '';
 
+  // submit via search button
+  $('#submit-btn').on('click', function (ev) {
+    ev.preventDefault();
+    SEARCH_STR = $('form input').val();
+    console.log('search string stored as: ' + SEARCH_STR);
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + SEARCH_STR,
+      dataType: 'jsonp',
+      type: 'GET',
+      success: loadResults
+    });
+  });
+
+  function loadResults (data) {
+    console.log(data);
+  }
 });
