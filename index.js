214bde7ac0cd3a1388d38868c35f2c7355581ad8
nickangtc <nickangtc@gmail.com>
2016-09-17 23:25:20 +0800

    added console logs to routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var session = require('express-session');
-var passport = require('./config/ppConfig'); // require('passport') done in ppConfig
+// var passport = require('./config/ppConfig'); // require('passport') done in ppConfig
 
 // ============= MIDDLE WARE + CONFIGURATIONS ==============
 
@@ -16,28 +16,32 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 app.use(ejsLayouts);
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'younotsupposedtoknow',
+  secret: process.env.SESSION_SECRET || 'canthashthis',
   resave: false,
   saveUninitialized: true
 }));
 // initialize the passport configuration and session as middleware
-app.use(passport.initialize());
-app.use(passport.session()); // this must come after use(session) - dependency
+// app.use(passport.initialize());
+// app.use(passport.session()); // this must come after use(session) - dependency
 
 // ============== ROUTES =============
 // READ: get homepage
 app.get('/', function (req, res) {
+  console.log('GET / request received');
   res.render('index');
 });
 
 // READ: get new user creation form
 app.get('/users/new', function (req, res) {
+  console.log('GET /users/new request received');
   // render user_new
   res.render('user_new');
 });
 
 // CREATE: register new user
 app.post('/users', function (req, res) {
+  console.log('POST /users request received');
+  console.log('req.body:', req.body);
   // if successful (validated, no matches in db)
   // ^ redirect to get '/'
 
@@ -45,25 +49,30 @@ app.post('/users', function (req, res) {
   // ^ redirect to get '/users/new' with flash msg
 
   // if user exists - redirect to 'login'
-  console.log('req:', req);
-  console.log('req.body:', req.body);
   res.render('index');
 });
 
 // READ: get profile
 app.get('/users/:id/starred', function (req, res) {
+  console.log('GET /users/id request received');
+  console.log('id:', req.params.id);
   // render user_starred
   res.render('user_starred');
 });
 
 // READ: get user profile edit form
 app.get('/users/:id/edit', function (req, res) {
+  console.log('GET /users/id/edit request received');
+  console.log('id:', req.params.id);
   // render user_profile_edit
   res.render('user_profile_edit');
 });
 
 // UPDATE: update user profile
 app.put('/users/:id', function (req, res) {
+  console.log('PUT /users/id request received');
+  console.log('id:', req.params.id);
+  console.log('req.body:', req.body);
   // update db
 
   // if successful (validated)
