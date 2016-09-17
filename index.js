a79dcd574322c2377364dad88c7e5581b471aac1
nickangtc <nickangtc@gmail.com>
2016-09-17 14:03:21 +0800

    created 7 basic REST routes, added express-ejs-layouts

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 
 // ============= MIDDLE WARE + CONFIGURATIONS ==============
 
 // app.use(express.static(__dirname + '/static'));
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-app.set('view engine', 'ejs');
+app.use(ejsLayouts);
 
 // ============== ROUTES =============
+// READ: get homepage
 app.get('/', function (req, res) {
   res.render('index');
 });
 
+// READ: get new user creation form
+app.get('/users/new', function (req, res) {
+
+});
+
+// CREATE: register new user
 app.post('/users', function (req, res) {
   console.log('req:', req);
   console.log('req.body:', req.body);
   res.render('index');
 });
 
-app.get('/greet/:name/:lastname', function (req, res) {
-  res.send('Hello ' + req.params.name + ' ' + req.params.lastname);
+// READ: get profile
+app.get('/users/:id', function (req, res) {
+
 });
 
-app.get('/multiply/:x/:y', function (req, res) {
-  res.send('The answer is: ' + (req.params.x * req.params.y));
+// READ: get user profile edit form
+app.get('/users/:id/edit', function (req, res) {
+
 });
 
-app.get('/add/:x/:y', function (req, res) {
-  res.send('The answer is: ' + (parseInt(req.params.x) + parseInt(req.params.y)));
+// UPDATE: update user profile
+app.put('/users/:id', function (req, res) {
+
+});
+
+// DESTROY: delete user account
+app.delete('/users/:id', function (req, res) {
+
 });
 
 app.listen(3000);
