00cd76079a7c6ce574c7810df869d27a18026275
nickangtc <nickangtc@gmail.com>
2016-09-18 20:30:40 +0800

    generated sequelize migration and model for savedUrl

---
+++ migrations/20160918122249-create-saved-url.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('savedUrls', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('savedUrls');
+  }
+};
---
+++ models/savedurl.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var savedUrl = sequelize.define('savedUrl', {
+    userid: DataTypes.INTEGER,
+    url: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return savedUrl;
+};
